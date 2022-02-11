c1aeb0640839b4f33dc64a62bd2a9e8ba1db65df
Stoozy <mahinsemail@gmail.com>
2022-02-11 00:06:12 -0500

    Ported newlib

--- bootleg_toolchain.sh
+++ bootleg_toolchain.sh
@@ -1,14 +1,14 @@
 CURRDIR=$(pwd)
  
 # make symlinks (a bad hack) to make newlib work
-#cd ~/cross/bin/ # this is where the bootstrapped generic cross compiler toolchain (i686-elf-xxx) is installed in,
+cd ~/opt/cross/bin/ # this is where the bootstrapped generic cross compiler toolchain (i686-elf-xxx) is installed in,
                 # change this based on your development environment.
-cd /usr/bin
-ln ar x86_64-atlas-ar
-ln as x86_64-atlas-as
-ln gcc x86_64-atlas-gcc
-ln gcc x86_64-atlas-cc
-ln ranlib x86_64-atlas-ranlib
+#cd /usr/bin
+ln x86_64-unknown-elf-ar x86_64-atlas-ar
+ln x86_64-unknown-elf-as x86_64-atlas-as
+ln x86_64-unknown-elf-gcc x86_64-atlas-gcc
+ln x86_64-unknown-elf-gcc x86_64-atlas-cc
+ln x86_64-unknown-elf-ranlib x86_64-atlas-ranlib
  
 # return
 cd $CURRDIR
--- newlib-2.5.0/newlib/libc/sys/atlas/syscalls.c
+++ newlib-2.5.0/newlib/libc/sys/atlas/syscalls.c
@@ -7,8 +7,25 @@
 #include <sys/time.h>
 #include <stdio.h>
  
-void _exit();
-int close(int file);
+typedef unsigned long long u64;
+
+void _exit(){
+    asm volatile("mov $0, %rdi\n\t\ 
+        syscall");
+}
+
+int close(int file){
+    u64 r8 = (u64)file;
+    asm volatile("mov $2, %%rdi\n\t\ 
+            mov %0, %%r8\n\t\
+            syscall" :"=r"(r8));
+
+    u64 r15;
+    asm volatile("mov %%r15, %0": : "r"(r15));
+
+    return (int)r15;
+}
+
 char **environ; /* pointer to array of char * strings that define the current environment variables */
 int execve(char *name, char **argv, char **env);
 int fork();
@@ -18,8 +35,38 @@ int isatty(int file);
 int kill(int pid, int sig);
 int link(char *old, char *new);
 int lseek(int file, int ptr, int dir);
-int open(const char *name, int flags, ...);
-int read(int file, char *ptr, int len);
+
+int open(const char *name, int flags, ...){
+    u64 r8 = (u64)name;
+    u64 r9 = (u64)flags;
+    asm volatile("mov $2, %%rdi\n\t\ 
+            mov %0, %%r8\n\t\
+            mov %1, %%r9\n\t\
+            syscall" :"=r"(r8),"=r"(r9));
+
+    u64 r15;
+    asm volatile("mov %%r15, %0": : "r"(r15));
+
+    return (int)r15;
+
+}
+
+int read(int file, char *ptr, int len){
+    u64 r8 = (u64)file;
+    u64 r9 = (u64)ptr;
+    u64 r10 = (u64) len;
+    asm volatile("mov $2, %%rdi\n\t\ 
+            mov %0, %%r8\n\t\
+            mov %1, %%r9\n\t\
+            mov %2, %%r10\n\t\
+            syscall" :"=r"(r8),"=r"(r9),"=r"(r10));
+
+    u64 r15;
+    asm volatile("mov %%r15, %0": : "r"(r15));
+
+    return (int)r15;
+}
+
 caddr_t sbrk(int incr);
 int stat(const char *file, struct stat *st);
 clock_t times(struct tms *buf);
