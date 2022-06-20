9e1e64da4c84cf40e60ce7f20c50be3e04bebfea
Stoozy <mahinsemail@gmail.com>
2022-06-19 23:33:36 -0400

    reworking vfs

--- kernel/src/fs/vfs.c
+++ kernel/src/fs/vfs.c
@@ -1,104 +1,101 @@
 #include "vfs.h"
+#include "../string/string.h"
 #include "../kmalloc.h"
-#include "../kprintf.h"
-
-static FileSystem * g_filesystems[VFS_MAX_DEVICES] = {0}; 
 
 
+VfsNode * gp_root;
+VfsOpenListNode * gp_open_list;
 
-/* path here must be absolute */
-static u16 get_device_index_from_path(const char * path){
-    u64 index =  (((path[0] - 'a') * 10) + (path[1] - 48));
-    return (u16)index;
+
+static VfsNode * children_from_fs(FileSystem * fs, u64 * count){
+    /* TODO */
+    return NULL;
 }
 
-void vfs_register_fs(FileSystem  * new_fs, u16 device_id){
-    char device[3];
-    device[0] =  'a' + (device_id / 10);
-    device[1] =  '0'+device_id % 10;
-    device[2] = '\0';
+static VfsNode * vfs_node_from_path(const char * path){
+    /* TODO */
+    return NULL;
+}
 
-    kprintf("[VFS]  Registering %s on index %d\n", device, device_id);
+File * vfs_open(const char * filename , int flags){
+    VfsNode * node = vfs_node_from_path(filename);
 
-    if(device_id < VFS_MAX_DEVICES && new_fs)
-        g_filesystems[device_id] = new_fs;
+    if(node)
+        return node->fs->open(filename, flags);
+
+    return NULL;
+
+}
+
+void vfs_close(File file){
+    VfsOpenListNode * current_node = gp_open_list;
+    
+    while(current_node->next != NULL){
+        if(strcmp(current_node->next->vfs_node->file->name, file.name) == 0){
+            VfsOpenListNode * free_this = current_node->next;
+
+            /* relink here */
+            VfsOpenListNode * new_next = current_node->next->next;
+            current_node->next = new_next;
+
+            kfree(free_this);
+        }
+    }
+
+}
+
+u64 vfs_read(File * file, u64 size, u8 * buffer){
+    /* TODO */
+    return 0;
+}
+
+u64 vfs_write(File * file, u64 size, u8 * buffer){
+    /* TODO */
+    return 0;
+}
+
+
+void vfs_register_fs(const char * path, FileSystem * fs, u64 device_id){
+    VfsNode * node = vfs_node_from_path(path);
+
+    memset(node, 0, sizeof(VfsNode));
+
+    node->file->type = VFS_MOUNTPOINT;
+    node->file->device = device_id;
+
+    node->fs = fs;
+
+    /* TODO: populate children based on actual fs */
+
+    u64 num_children;
+
+    node->children = children_from_fs(fs, &num_children);
+    node->num_children = num_children;
+
 
     return;
 }
 
 void vfs_unregister_fs(FileSystem * fs){
-    for(u16 fs_idx=0; fs_idx < VFS_MAX_DEVICES; ++fs_idx){
-        if(g_filesystems[fs_idx] == fs){
-            // free some memory
-            kfree(fs->name);
-            kfree(fs);
 
-            // empty fs entry
-            g_filesystems[fs_idx] = 0;
-        }
-    }
+    /* TODO */
+    return;
 }
 
+void vfs_init(FileSystem * root_fs){
+    gp_root = kmalloc(sizeof(VfsNode));
+    gp_root->fs = root_fs;
 
-u64 vfs_read(FILE * file, u64 size, u8 * buffer){
-    u16 fs_idx = file->device; 
+    gp_root->file = kmalloc(sizeof(File));
+    gp_root->file->type = VFS_MOUNTPOINT;
+    gp_root->file->device = root_fs->device;
 
-    if(g_filesystems[fs_idx]){
-        u64 bytes_read  = g_filesystems[fs_idx]->read(file, size, buffer);
-        return bytes_read;
-    }
+    u64 num_children;
 
-    // zero bytes read
-    return 0;
+    gp_root->children = children_from_fs(root_fs, &num_children);
+    gp_root->num_children = num_children;
 
+    return;
 }
 
-
-u64 vfs_write(FILE * file, u64 size, u8 * buffer){
-    u16 fs_idx = file->device;
-
-    if(g_filesystems[fs_idx]){
-        u64 bytes_written  = g_filesystems[fs_idx]->write(file, size, buffer);
-        return bytes_written;
-    }
-
-    // zero bytes written
-    return 0;
-
-}
-
-
-FILE * vfs_open(const char *filename, int flags){
-
-    kprintf("[VFS]  Filepath: %s Flags: %d\n", filename, flags);
-
-    if(filename){
-        if(filename[2] == ':'){
-            u16 fs_idx = get_device_index_from_path(filename);
-
-            // skip device identifier 
-            // e.g. a0:/foo/bar => /foo/bar
-            filename += 3;
-            if(g_filesystems[fs_idx]){
-                kprintf("Got fs_idx %d\n", fs_idx);
-                FILE * file = g_filesystems[fs_idx]->open(filename, flags);
-                if(file == NULL)
-                    return NULL;
-
-                kprintf("[VFS]  Opened: %s\n", file->name);
-                kprintf("File inode %llx\n", file->inode);
-                file->device = fs_idx;
-                return file;
-            }
-        }
-    }
-
-    FILE * f = kmalloc(sizeof(FILE));
-    f->mode = VFS_INVALID_FS;
-    return f;
-}
-
-void vfs_close(FILE file){
-    // TODO
-}
 
--- kernel/src/fs/vfs.h
+++ kernel/src/fs/vfs.h
@@ -13,46 +13,79 @@
 #define VFS_MOUNTPOINT      0x08
 #define VFS_INVALID_FS      0x09
 
-#define VFS_MAX_DEVICES     260
-
 struct file;
 
-typedef void (*mount)();
-typedef struct file * (*open_func_t)(const char * filename, int flags);
+typedef struct sector_node {
+    u64 sector;
+    struct sector_node * next;
+} SectorNode;
+
+
+
+typedef struct file {
+
+    char * name;
+    SectorNode * sectors;
+    u64 device;
+    u64 position;
+    u64 size;
+
+    u8  type;
+    u8  mode;
+
+} File;
+
+typedef File * (*open_func_t)(const char * filename, int flags);
 typedef void (*close_func_t)(struct file *);
 typedef u64 (*read_func_t)(struct file *, u64, u8 *);
 typedef u64 (*write_func_t)(struct file *, u64, u8 *);
 typedef struct file * (*finddir_func_t)(const char * dirname);
 
 
-typedef struct file {
-    char * name;
-    u64 device;
-    u64 inode;
-    u64 size;
-    u64 position;
-    u64 mode;
-    u64 eof;
-    u64 sector;
-} FILE;
 
 typedef struct fs {
+
     char * name;
+
+    u64 device;
     open_func_t     open;
     close_func_t    close;
     read_func_t     read;
     write_func_t    write;
     finddir_func_t  finddir; 
+
 } FileSystem;
 
 
-FILE * vfs_open(const char * filename , int flags);
-void vfs_close(FILE file);
-u64 vfs_read(FILE * file, u64 size, u8 * buffer);
-u64 vfs_write(FILE * file, u64 size, u8 * buffer);
+typedef struct vfs_node {
+    
 
-void vfs_register_fs(FileSystem *, u16 device_id);
+    FileSystem * fs;
+    File * file;
+
+    u64 num_children;
+    struct vfs_node * children;
+
+}   VfsNode;
+
+
+typedef struct open_list_node {
+
+    VfsNode * vfs_node;
+    struct open_list_node * next;
+
+} VfsOpenListNode;
+
+
+
+File * vfs_open(const char * filename , int flags);
+void vfs_close(File file);
+
+u64 vfs_read(File * file, u64 size, u8 * buffer);
+u64 vfs_write(File * file, u64 size, u8 * buffer);
+
+void vfs_register_fs( const char * path ,FileSystem *, u64 device_id);
 void vfs_unregister_fs(FileSystem * fs);
 
-void vfs_init();
+void vfs_init(FileSystem *);
 
