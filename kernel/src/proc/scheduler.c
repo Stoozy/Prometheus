ede2c8231609fb93bd2b8f6d251922a91b822789
Stoozy <mahinsemail@gmail.com>
2022-07-21 21:53:22 -0400

    Got rid of unnecessary 'save_context' function

--- kernel/src/proc/scheduler.c
+++ kernel/src/proc/scheduler.c
@@ -11,42 +11,6 @@ extern ProcessControlBlock *gp_process_queue;
 
 extern PageTable *kernel_cr3;
 
-void save_context(ProcessControlBlock *proc, Registers *regs) {
-
-  kprintf("[SCHDULER] Saving context: \n");
-  dump_regs(regs);
-
-  u64 *stack = (u64 *)(regs->rsp) ;
-
-  *--stack = regs->ss;
-  *--stack = (u64)proc->p_stack;
-  *--stack = regs->rflags;
-  *--stack = regs->cs;
-  *--stack = regs->rip;
-
-  *--stack = regs->r8;
-  *--stack = regs->r9;
-  *--stack = regs->r10;
-  *--stack = regs->r11;
-  *--stack = regs->r12;
-  *--stack = regs->r13;
-  *--stack = regs->r14;
-  *--stack = regs->r15;
-
-  *--stack = regs->rbp;
-
-  *--stack = regs->rdx;
-  *--stack = regs->rcx;
-  *--stack = regs->rbx;
-  *--stack = regs->rax;
-  *--stack = regs->rsi;
-  *--stack = regs->rdi;
-
-  proc->p_stack = stack;
-
-  return;
-}
-
 void schedule(Registers *regs) {
 
   // load_pagedir(kernel_cr3);
@@ -59,7 +23,7 @@ void schedule(Registers *regs) {
     return;
   }
 
-  save_context(gp_current_process, regs);
+  gp_current_process->p_stack = (void*)regs;
 
   if (gp_current_process->next == NULL) {
     gp_current_process = gp_process_queue; // go to head
