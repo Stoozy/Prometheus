fa448491d7acf177ee834915ee792cc3433ac9c2
Stoozy <stoozy@proton.me>
2022-07-31 12:11:04 -0400

    Got rid of untracked files from prev commits

--- sysroot/lib/internal-config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define MLIBC_SYSTEM_NAME "atlas"
-#undef MLIBC_MAP_DSO_SEGMENTS
-#undef MLIBC_MAP_FILE_WINDOWS
-/* #undef MLIBC_STATIC_BUILD */
-
--- sysroot/usr/include/complex.h
+++
@@ -1,4 +0,0 @@
-#ifndef _COMPLEX_H
-#define _COMPLEX_H
-
-#endif // _COMPLEX_H
