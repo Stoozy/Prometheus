4040fef3afeebeb4304761b22e6dcd9b75949f80
stoozy <stoozy@proton.me>
2022-09-04 00:30:23 -0400

    Scheduler changes
    Just preparing for blocking/non-blocking
    processes

--- kernel/src/proc/proc.c
+++ kernel/src/proc/proc.c
@@ -1,4 +1,5 @@
 #include "cpu/cpu.h"
+#include "memory/vmm.h"
 #include <config.h>
 #include <drivers/video.h>
 #include <fs/vfs.h>
@@ -10,14 +11,57 @@
 #include <stdint.h>
 #include <string/string.h>
 
-ProcessControlBlock *gp_process_queue = NULL;
-ProcessControlBlock *gp_current_process = NULL;
-PageTable *kernel_cr3 = NULL;
-u64 g_procs = 0;
-
 extern u64 g_ticks;
 extern void switch_to_process(void *new_stack, PageTable *cr3);
 extern void load_pagedir();
+
+volatile ProcessQueue ready_queue = {0, NULL, NULL};
+volatile ProcessQueue wait_queue = {0, NULL, NULL};
+volatile ProcessControlBlock *running = NULL;
+
+PageTable *kernel_cr3 = NULL;
+
+void pqueue_insert(ProcessQueue *queue, ProcessControlBlock *proc) {
+  if (queue->first == NULL)
+    queue->first = queue->last = proc;
+
+  // append to last
+  queue->last->next = proc;
+  queue->last = queue->last->next;
+  queue->last->next = NULL;
+
+  ++queue->count;
+}
+
+void pqueue_remove(ProcessQueue *queue, ProcessControlBlock *proc) {
+  kprintf("Removing process at %x\n", proc);
+  if (queue->first == proc) {
+    if (queue->last == proc) {
+      queue->first = queue->last = NULL;
+      --queue->count;
+      proc->next = NULL;
+      return;
+    }
+
+    queue->first = queue->first->next;
+    --queue->count;
+    proc->next = NULL;
+    return;
+  }
+
+  ProcessControlBlock *cproc = queue->first;
+  while (cproc) {
+    if (cproc->next == proc) {
+      cproc->next = proc->next;
+      --queue->count;
+      proc->next = NULL;
+      return;
+    }
+    cproc = cproc->next;
+  }
+
+  return;
+}
 
 void unmap_fd_from_proc(ProcessControlBlock *proc, int fd) {
   if (fd > MAX_PROC_FDS || fd < 0)
@@ -41,33 +85,18 @@ int map_file_to_proc(ProcessControlBlock *proc, struct file *file) {
 }
 
 void kill_proc(ProcessControlBlock *proc) {
-
-  // Need kernel cr3 for kernel data structures...
-  load_pagedir(kernel_cr3);
-
-  ProcessControlBlock *pcb = gp_process_queue;
-
-  while (pcb->next != gp_current_process)
-    pcb = pcb->next;
-
-  // pcb->next now points to gp_current_process
-
-  ProcessControlBlock *node_to_remove = pcb->next;
-
-  // skip a node, and relink
-  pcb->next = gp_current_process->next;
-
-  // TODO: free memory
-
-  // decrease global number of procs
-  --g_procs;
+  // TODO
 }
 
-void kill_current_proc(void) { kill_proc(gp_current_process); }
+void kill_current_proc(void) { kill_proc(running); }
 
 void task_a() {
-  for (;;)
-    kprintf("Running task A...\n");
+  extern uint8_t kbd_read_from_buffer();
+  for (;;) {
+    kprintf("Running task A ...\n");
+    // uint8_t c = kbd_read_from_buffer();
+    // kprintf("Got character %c\n", c);
+  }
 }
 
 void task_b() {
@@ -79,16 +108,12 @@ void idle_task() {
     kprintf("Idling...\n");
 }
 
-void dump_list() {
-  kprintf("---------PROCESS QUEUE---------\n");
-  volatile ProcessControlBlock *current = gp_process_queue;
-
-  kprintf("0x%x ->", current);
-  while (current->next != NULL) {
-    current = current->next;
-    kprintf("0x%x ->", current);
+void dump_queue(ProcessQueue *pqueue) {
+  ProcessControlBlock *cproc = pqueue->first;
+  kprintf("There are %d processes in this queue\n", pqueue->count);
+  for (; cproc; cproc = cproc->next) {
+    kprintf("Process at %x\n", cproc);
   }
-  kprintf(" NULL\n");
 }
 
 void dump_proc_vas(ProcessControlBlock *proc) {
@@ -137,6 +162,7 @@ ProcessControlBlock *create_kernel_process(void (*entry)(void)) {
   pcb->trapframe.rip = (uint64_t)entry;
 
   pcb->cr3 = vmm_get_current_cr3(); // kernel cr3
+  pcb->state = READY;
   pcb->next = NULL;
 
   return pcb;
@@ -151,11 +177,6 @@ ProcessControlBlock *clone_process(ProcessControlBlock *proc, Registers *regs) {
   for (int i = 0; i < proc->fd_length; i++) {
     File *copy_file = kmalloc(sizeof(File));
     *copy_file = *(proc->fd_table[i]);
-
-    // copy_file->name = kmalloc(strlen(proc->fd_table[i]->name) + 1);
-    // strcpy(copy_file->name, proc->fd_table[i]->name);
-    // copy_file->name[strlen(proc->fd_table[i]->name)] = '\0';
-
     clone->fd_table[i] = copy_file;
   }
 
@@ -175,40 +196,32 @@ ProcessControlBlock *clone_process(ProcessControlBlock *proc, Registers *regs) {
 }
 
 void register_process(ProcessControlBlock *new_pcb) {
+  pqueue_insert(&ready_queue, new_pcb);
+  return;
+}
 
-#ifdef SCHEDULER_DEBUG
-  kprintf("[TASKING]  Registering process at 0x%x\n", new_pcb);
-#endif
+void block_process(ProcessControlBlock *proc, int reason) {
+  // scheduler will handle the rest
+  // note: the rest isn't done here
+  // because the registers have to
+  // be saved once the interrupt fires
+  proc->state = reason;
+  return;
+}
 
-  volatile ProcessControlBlock *current_pcb = gp_process_queue;
-
-  kprintf("Current pcb is at %x\n", current_pcb);
-
-  // first process
-  if (!current_pcb) {
-    gp_process_queue = new_pcb;
-    ++g_procs;
-    return;
-  }
-
-  while (current_pcb->next != NULL)
-    current_pcb = current_pcb->next;
-  current_pcb->next = new_pcb;
-  new_pcb->next = NULL;
-
-#ifdef SCHEDULER_DEBUG
-  kprintf("[TASKING]  PCB at 0x%x is after 0x%x\n", current_pcb->next,
-          current_pcb);
-  kprintf("[TASKING]  PCB has next 0x%x\n", current_pcb->next);
-#endif
-
-  ++g_procs;
+void unblock_process(ProcessControlBlock *proc) {
+  proc->state = READY;
+  pqueue_remove(&wait_queue, proc);
+  pqueue_insert(&ready_queue, proc);
   return;
 }
 
 void multitasking_init() {
   // save kernel page tables
   kernel_cr3 = vmm_get_current_cr3();
+
+  memset(&ready_queue, 0, sizeof(ProcessQueue));
+  memset(&wait_queue, 0, sizeof(ProcessQueue));
 
   char *envp[5] = {"SHELL=/usr/bin/bash", "PATH=/usr/bin", "HOME=/",
                    "TERM=linux", NULL};
@@ -219,14 +232,25 @@ void multitasking_init() {
   // kprintf("Got process at %x\n", nomterm);
   // register_process(nomterm);
 
-  extern void refresh_screen_proc();
-  ProcessControlBlock *video_refresh =
-      create_kernel_process(refresh_screen_proc);
-  register_process(video_refresh);
-  gp_current_process = gp_process_queue;
+  register_process(create_kernel_process(idle_task));
+  register_process(create_kernel_process(task_b));
+  register_process(create_kernel_process(task_a));
 
-  kprintf("switching to process pid:%d\n", gp_current_process->pid);
-  switch_to_process(&gp_current_process->trapframe,
-                    (void *)gp_current_process->cr3);
+  // extern void terminal_main();
+  // ProcessControlBlock *kterm = create_kernel_process(terminal_main);
+  // register_process(kterm);
+
+  // extern void refresh_screen_proc();
+  // ProcessControlBlock *video_refresh =
+  //     create_kernel_process(refresh_screen_proc);
+  // register_process(video_refresh);
+
+  running = ready_queue.first;
+  kprintf("Ready queue has %d procs\n", ready_queue.count);
+  dump_queue(&ready_queue);
+  dump_queue(&wait_queue);
+
+  kprintf("switching to process pid:%d\n", running->pid);
+  switch_to_process(&running->trapframe, (void *)running->cr3);
   return;
 }
--- kernel/src/proc/scheduler.c
+++ kernel/src/proc/scheduler.c
@@ -1,51 +1,54 @@
+#include "memory/vmm.h"
 #include <config.h>
+#include <kmalloc.h>
 #include <kprintf.h>
 #include <proc/proc.h>
 
 extern void switch_to_process(void *new_stack, PageTable *cr3);
 extern void load_pagedir();
+
 extern u64 g_ticks;
-extern u64 g_procs;
-extern ProcessControlBlock *gp_current_process;
-extern ProcessControlBlock *gp_process_queue;
 
 extern PageTable *kernel_cr3;
 
+extern ProcessQueue ready_queue;
+extern ProcessQueue wait_queue;
+extern ProcessControlBlock *running;
+
 void schedule(Registers *regs) {
+  // not enough procs
+  if (ready_queue.count == 0)
+    return;
 
-  // not enough procs or not time to switch yet
-  if (g_procs == 0) {
-    return;
-  } else if (g_ticks % SMP_TIMESLICE != 0) {
-    // load_pagedir(gp_current_process->cr3);
-    return;
+  // save registers
+  running->trapframe = *regs;
+
+  if (running->state == WAITING) {
+    pqueue_remove(&ready_queue, running);
+    pqueue_insert(&wait_queue, running);
   }
 
-  gp_current_process->trapframe = *regs;
+  if (running->next == NULL)
+    running = ready_queue.first; // go to head
+  else
+    running = running->next; // move to next proc
 
-  if (gp_current_process->next == NULL) {
-    gp_current_process = gp_process_queue; // go to head
-
-#ifdef SCHEDULER_DEBUG
-    kprintf("[SCHEDULER] Switching to head:\n");
-    dump_regs(&gp_current_process->trapframe);
-#endif
-
-  } else if (gp_current_process->next != NULL) {
-    // move to next proc
-    gp_current_process = gp_current_process->next;
-#ifdef SCHEDULER_DEBUG
-    kprintf("[SCHEDULER] Switching to next: \n");
-    dump_regs(&gp_current_process->trapframe);
-#endif
-  }
+  running->state = RUNNING;
 
 #ifdef SCHEDULER_DEBUG
-  kprintf(
-      "[SCHEDULER]    Switching to proc with stack at %llx and %llx as cr3\n",
-      gp_current_process->trapframe, (void *)gp_current_process->cr3);
+  extern void dump_queue(ProcessQueue *);
+
+  kprintf("Ready Queue\n");
+  dump_queue(&ready_queue);
+
+  kprintf("Wait Queue\n");
+  dump_queue(&wait_queue);
+
+  kprintf("[SCHEDULER]    Switching to proc with trapframe at %llx and %llx as "
+          "cr3\n",
+          running->trapframe, (void *)running->cr3);
 #endif
 
-  switch_to_process(&gp_current_process->trapframe,
-                    (void *)gp_current_process->cr3);
+  // finally, switch
+  switch_to_process(&running->trapframe, (void *)running->cr3);
 }
